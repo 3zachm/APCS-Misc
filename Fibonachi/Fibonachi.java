2e2ba9b4927093dbfa9b5e97067c079a041c223a
3zachm <3zachm4@gmail.com>
2020-02-19 12:13:58 -0800

    Simple recursion assignment

---
+++ Fibonachi/Fibonachi.java
@@ -0,0 +1,14 @@
+public class Fibonachi {
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+    }
+
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
